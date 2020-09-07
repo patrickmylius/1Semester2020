0fd63785f3eb6de50493502379e842beff6c1361
Patrick <patr6996@edu.easj.dk>
2020-09-07 14:38:50 +0200

    Mapper til undervisnings sortering.

--- src/Undervisning_07_09_2020/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package Undervisning_07_09_2020;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello Mom");
-        System.out.println(5 + 5);
-    }
-}
