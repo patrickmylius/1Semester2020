85a303902d47c954a42cdd190c46f175834a6420
Patrick <patr6996@edu.easj.dk>
2020-09-09 10:58:44 +0200

    Tilføjede til Mappen SystemUdvikling>SelektionsOpgaver>Src> Opgave 1 og 2

--- SystemUdvikling/SelektionsOpgaver/src/Opgave3.java
+++ SystemUdvikling/SelektionsOpgaver/src/Opgave3.java
@@ -1,2 +1,10 @@
 public class Opgave3 {
+  //TEST CASE Lav et program der:
+    //Erklærer to variable first og second.
+    //Sætter first og second til værdier du selv vælger
+    //Undersøger hvilken af de to variable der har den største værdi og udskriver denne
+    //Test dit program med forskellige værdier for first og second.
+  public static void main(String[] args) {
+
+  }
 }
