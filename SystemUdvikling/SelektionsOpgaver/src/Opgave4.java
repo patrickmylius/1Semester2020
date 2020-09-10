fa4e8ef4e0ce5fd71bebdd5829a03e6f00d749c7
Patrick <patr6996@edu.easj.dk>
2020-09-10 16:23:32 +0200

    Opgave løsning med testcase til opgave 4. (Udvidet med en third variable til opgave 3).

--- SystemUdvikling/SelektionsOpgaver/src/Opgave4.java
+++ SystemUdvikling/SelektionsOpgaver/src/Opgave4.java
@@ -1,2 +1,54 @@
 public class Opgave4 {
+    /*Lav en udvidelse af din løsning til den foregående opgave,
+    så den kan finde den største af tre variable,
+     kald den tredie variabel third.*/
+    public static void main(String[] args) {
+            //Input: first = 25, second = 27, third = 45
+            //forventet output: "45 + third variable are the highest"
+            int first = 25;
+            int second = 27;
+            int third = 45;
+            if (first > second && first > third)
+                System.out.println(first + " first variable are the highest");
+            if (second > first && second > third)
+                System.out.println(second + " second variable are the highest");
+            if (third > first && third > second)
+                System.out.println(third + " third variable are the highest");
+            if (first == second && first == third)
+                System.out.println(first + " and " + second + " and " + third + " are all equal ");
+            //faktisk output: "45 + third variable are the highest"
+            //testresultat: pass
+
+            //Input: first = 2354234, 2325342, third = 2334555
+            //forventet output = "first variable is higher than the second variable"
+            first = 2354234;
+            second = 2325342;
+            third = 2334555;
+            if (first > second && first > third)
+            System.out.println(first + " first variable are the highest");
+            if (second > first && second > third)
+            System.out.println(second + " second variable are the highest");
+            if (third > first && third > second)
+            System.out.println(third + " third variable are the highest");
+            if (first == second && first == third)
+            System.out.println(first + " and " + second + " and " + third + " are all equal ");
+            //faktisk output "2354234 first variable are the highest"
+            //testresultat: pass
+
+            //Input: first = 500, second = 500, third = 600
+            //forventet output: "500 first 500 second 500 third variables, are all equal"
+            first = 500;
+            second = 500;
+            third = 500;
+            if (first > second && first > third)
+            System.out.println(first + " first variable are the highest");
+            if (second > first && second > third)
+            System.out.println(second + " second variable are the highest");
+            if (third > first && third > second)
+            System.out.println(third + " third variable are the highest");
+            if (first == second && first == third)
+            System.out.println(first + " first " + second + " second " + third + " third variables, are all equal ");
+            //forventet output: "500 first 500 second 500 third variables, are all equal"
+            //testresultat: pass
+    }
 }
