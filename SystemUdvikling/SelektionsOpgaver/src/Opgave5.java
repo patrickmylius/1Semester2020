fee949bb4811e655ac2f9f483e63cd882290b4e3
Patrick <patr6996@edu.easj.dk>
2020-09-10 16:25:06 +0200

    Prepped Opgave5

--- SystemUdvikling/SelektionsOpgaver/src/Opgave5.java
+++ SystemUdvikling/SelektionsOpgaver/src/Opgave5.java
@@ -1,2 +1,12 @@
 public class Opgave5 {
+    /* I denne opgave skal vi arbejde med intervaller.
+    Lav et program der:
+    Erklærer en variable x.
+    Sætter x til en værdi i intervallet [0:99] som du selv vælger.
+    Udskriver hvilket "tier-interval" (0..9, 10..19, ... , 90..99) x tilhører.
+
+    F.eks. skal programmet udskrive:
+    62 tilhører [60:69]
+    hvis x er 62
+    Test dit program med forskellige værdier for x.*/
 }
