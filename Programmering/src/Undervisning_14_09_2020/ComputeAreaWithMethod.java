e429a18f13bfcab0664187990ee5e2343d53f2f3
Patrick <patr6996@edu.easj.dk>
2020-09-14 10:03:04 +0200

    Tilføjet klasse med ComputeAreaWithMethod

---
+++ Programmering/src/Undervisning_14_09_2020/ComputeAreaWithMethod.java
@@ -0,0 +1,15 @@
+package Undervisning_14_09_2020;
+
+public class ComputeAreaWithMethod {
+    public static void main(String[] args) {
+        //Souts metoden med given input 2, 5 , 10.
+        System.out.println("Arealet for radius 2 er " + beregnAreal(2));
+        System.out.println("Arealet for radius 5 er " + beregnAreal(5));
+        System.out.println("Arealet for radius 10 er " + beregnAreal(10));
+    }
+    //Beregn areal metoden.
+    public static double beregnAreal(double radius) {
+        double areal = radius * radius * 3.14;
+        return areal;
+    }
+}
