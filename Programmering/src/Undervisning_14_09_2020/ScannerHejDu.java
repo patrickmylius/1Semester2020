a9819c006ed44669bb9de7d11f0db7ceb030582a
Patrick <patr6996@edu.easj.dk>
2020-09-14 11:17:36 +0200

    Tilføjet klasse ScannerHejDu fra undervisningstimen

---
+++ Programmering/src/Undervisning_14_09_2020/ScannerHejDu.java
@@ -0,0 +1,14 @@
+package Undervisning_14_09_2020;
+import java.util.Scanner;
+public class ScannerHejDu {
+    public static void main(String[] args) {
+        //sets Scanner to input
+        Scanner input = new Scanner(System.in);
+        //souts user for an input in console
+        System.out.println("Hvad er dit navn?: ");
+        //sets users input to String navn
+        String navn = input.next();
+        //souts "Hej" + user input
+        System.out.println("Hej " + navn);
+    }
+}
