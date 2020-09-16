7b3e614c3d73d5659b56470b050b8fff1ab5830c
patrickmylius <patrickmylius@outlook.dk>
2020-09-16 21:37:51 +0200

    klasse DisplayTime.java refaktoreret.

---
+++ Programmering/src/Kodeværksted_Refaktorering_02/visTid.java
@@ -0,0 +1,21 @@
+package Kodeværksted_Refaktorering_02;
+// 1. Gennemgå og forstå koden.
+// 2. Refaktorer klassens navn, variable, metoder og tekster til dansk.
+// 3. Træk klassens funktionalitet ud i nye metode(r).
+// 4. Kald dine nye metoder fra main() - så vi kan se, at det rent faktisk virker.
+
+import java.util.Scanner;
+
+public class visTid {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // souter bruger efter et input
+        System.out.print("Indtast et tal for sekunder: ");
+        int sekunder = input.nextInt();
+
+        int minutter = sekunder / 60; // udregner minutter i sekunder
+        int resterendeSekunder = sekunder % 60; // resterende sekunder
+        System.out.println(sekunder + " sekunder er " + minutter +
+                " minutter og " + resterendeSekunder + " sekunder");
+    }
+}
