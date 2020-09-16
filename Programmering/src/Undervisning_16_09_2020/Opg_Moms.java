261b107f6176a89017a7b6ed2654bc13bcb78a40
patrickmylius <patrickmylius@outlook.dk>
2020-09-16 13:16:33 +0200

    Refactoring 3: Lav en ny metode, der kan beregene tysk moms (16%) plusTyskMoms(). + upgrades
    m. noter

--- Programmering/src/Undervisning_16_09_2020/Opg_Moms.java
+++ Programmering/src/Undervisning_16_09_2020/Opg_Moms.java
@@ -9,16 +9,38 @@ import java.util.Scanner;
 
 public class Opg_Moms {
     public static void main(String[] args) {
-        System.out.print("Indtast beløb: ");
-        plusMoms();
-
-    }
-    static void plusMoms(){
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Ønsker du at beregne tysk eller dansk moms: ");
+        String svar = input.nextLine();
+        //if loop som kører plusMoms metoden hvis input er lig dansk
+        if (svar.equalsIgnoreCase("dansk")) {
+            plusMoms();
+        }
+        //if loop som kører plusTyskMoms metoden hvis input er lig tysk
+        if (svar.equalsIgnoreCase("tysk")) {
+            plusTyskMoms();
+        }
+    }
+
+    //Metode plusMoms, bruger beløb til at beregne moms
+    static void plusMoms() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Indtast beløb: ");
         double beløb = input.nextDouble();
         double moms = beløb * 0.25;
         System.out.println("Moms udgør kr: " + (int) (moms * 100) / 100.0);
+    }
+
+    //Metode til tysk moms beregning, tager input og beregner tysk moms
+    static void plusTyskMoms() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Menge eingeben: ");
+        double beløb = input.nextDouble();
+        double moms = beløb * 0.16;
+        System.out.println("Mehrwertsteuer bildet: " + (int) (moms * 100) / 100.0);
     }
 
 }
