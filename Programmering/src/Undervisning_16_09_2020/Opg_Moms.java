c16eb656ca97cefe98536d0be8e008171afca804
patrickmylius <patrickmylius@outlook.dk>
2020-09-16 12:32:58 +0200

    klasse navn, variabler og souts refactoctoreret til dansk

--- Programmering/src/Undervisning_16_09_2020/Opg_Moms.java
+++ Programmering/src/Undervisning_16_09_2020/Opg_Moms.java
@@ -7,14 +7,14 @@ package Undervisning_16_09_2020;
 
 import java.util.Scanner;
 
-public class Opg_SalexTax {
+public class Opg_Moms {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter purchase amount: ");
-        double purchaseAmount = input.nextDouble();
+        System.out.print("Indtast beløb: ");
+        double beløb = input.nextDouble();
 
-        double tax = purchaseAmount * 0.06;
-        System.out.println("Sales tax is $: " + (int) (tax * 100) / 100.0);
+        double moms = beløb * 0.25;
+        System.out.println("Moms udgør kr: " + (int) (moms * 100) / 100.0);
     }
 }
