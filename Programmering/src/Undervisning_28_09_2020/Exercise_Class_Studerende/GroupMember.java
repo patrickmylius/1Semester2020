29d2ffdb6acbf42f0cb922879a5b81d510d35734
Patrick <patr6996@edu.easj.dk>
2020-09-26 23:08:50 +0200

    Started Method using objects exercise.

---
+++ Programmering/src/Undervisning_28_09_2020/Exercise_Class_Studerende/GroupMember.java
@@ -0,0 +1,8 @@
+package Undervisning_28_09_2020.Exercise_Class_Studerende;
+
+public class GroupMember {
+    String name;
+    int age;
+    String favoritFood;
+    String favoritTopic;
+}
---
+++ Programmering/src/Undervisning_28_09_2020/Exercise_Class_Studerende/Main.java
@@ -0,0 +1,26 @@
+package Undervisning_28_09_2020.Exercise_Class_Studerende;
+
+public class Main {
+    public static void main(String[] args) {
+
+        GroupMember memb1 = new GroupMember();
+        memb1.name = "Patrick";
+        memb1.age = 26;
+        memb1.favoritTopic = "Muscle training";
+        memb1.favoritFood = "Lasagne";
+
+        GroupMember memb2 = new GroupMember();
+        memb2.name = "Usamah";
+        memb2.age = 25;
+        memb2.favoritTopic = "Humanbeings behaviour";
+        memb2.favoritFood = "Noobs in league of legends";
+
+        GroupMember memb3 = new GroupMember();
+        memb3.name = "Daniella";
+        memb3.age = 25;
+        memb3.favoritTopic = "Jehova";
+        memb3.favoritFood = "Pizza";
+
+
+    }
+}
