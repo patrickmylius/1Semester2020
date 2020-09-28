1f93e9eb8d84c4b6cf7aff4c61a66930341674fc
Patrick <patr6996@edu.easj.dk>
2020-09-28 11:45:56 +0200

    Made project folder for homeassignment TODO: Use constructers, and classes to build a smart idea for your own home.

---
+++ Programmering/src/Undervisning_28_09_2020/Exercise_SmartHome/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Developed by Patrick Grønvold
+ * patr6996@edu.easj.dk
+ * Student of Computer Science
+ * Zealand ErhvervsAkademi - 4700
+ * 28-09-2020
+ */
+package Undervisning_28_09_2020.Exercise_SmartHome;
+
+public class Main {
+}
