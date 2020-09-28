2e635f0c5a0dc339775484c878d95bca375df374
Patrick <patr6996@edu.easj.dk>
2020-09-28 10:04:06 +0200

    Tilføjet Constructer - GroupMember() to GroupMember class

--- Programmering/src/Undervisning_28_09_2020/Exercise_Class_Studerende/GroupMember.java
+++ Programmering/src/Undervisning_28_09_2020/Exercise_Class_Studerende/GroupMember.java
@@ -1,7 +1,11 @@
 package Undervisning_28_09_2020.Exercise_Class_Studerende;
 
 public class GroupMember extends StudentInfo {
-    void introduce(){
+    void introduce() {
         System.out.println("Hi there, i am studying computer science");
+    }
+
+    GroupMember() {
+        studieRetning = "Computer science";
     }
 }
--- Programmering/src/Undervisning_28_09_2020/Exercise_Class_Studerende/Main.java
+++ Programmering/src/Undervisning_28_09_2020/Exercise_Class_Studerende/Main.java
@@ -15,28 +15,24 @@ public class Main {
             GroupMember usamah = new GroupMember();
             usamah.name = "Usamah";
             usamah.age = 25;
-            usamah.studieRetning = "Datamatik";
             usamah.favoritTopic = "Humanbeings behaviour";
             usamah.favoritFood = "Noobs in league of legends";
 
             GroupMember daniella = new GroupMember();
             daniella.name = "Daniella";
             daniella.age = 23;
-            daniella.studieRetning = "Datamatik";
             daniella.favoritTopic = "Jehova";
             daniella.favoritFood = "Pizza";
 
             GroupMember mikkel = new GroupMember();
             mikkel.name = "Mikkel";
             mikkel.age = 22;
-            mikkel.studieRetning = "Datamatik";
             mikkel.favoritTopic = "Gaming";
             mikkel.favoritFood = "Italian dishes";
 
             GroupMember danijel = new GroupMember();
             danijel.name = "Danijel";
             danijel.age = 22;
-            danijel.studieRetning = "Datamatik";
             danijel.favoritTopic = "Coding";
             danijel.favoritFood = "Burger";
 
@@ -65,6 +61,7 @@ public class Main {
                     danijel.introduce();
                     System.out.println("My name is " + danijel.name);
                     System.out.println("I am " + danijel.age + " years old");
+                    System.out.println(danijel.studieRetning);
                     break;
                 default:
                     System.out.println("Sorry, no group member of Copy paste, matching your input");
