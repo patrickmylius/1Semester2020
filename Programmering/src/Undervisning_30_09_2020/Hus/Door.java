90a281bf0ec77f9242d6421adc95b6e6f0a9c3bc
patrickmylius <patrickmylius@outlook.dk>
2020-09-30 17:21:48 +0200

    Refractored

--- Programmering/src/Undervisning_30_09_2020/Hus/Door.java
+++ Programmering/src/Undervisning_30_09_2020/Hus/Door.java
@@ -1,29 +1,32 @@
 package Undervisning_30_09_2020.Hus;
+
 //Class holding Door
 //What a door can do
 public class Door {
     boolean locked;
     boolean closed;
 
-    public Door() {
-    }
-    void locked() {
-
-    }
-    void openLock(){
-        this.locked = false;
-    }
-
-    void Open(){
-        closed = false;
-    }
-    void close(){
-        closed = true;
-    }
-
-
     public Door(boolean locked, boolean closed) {
         this.locked = locked;
         this.closed = closed;
+    }
+
+    public Door() {
+    }
+
+    void locked() {
+
+    }
+
+    void openLock() {
+        this.locked = false;
+    }
+
+    void Open() {
+        closed = false;
+    }
+
+    void close() {
+        closed = true;
     }
 }
