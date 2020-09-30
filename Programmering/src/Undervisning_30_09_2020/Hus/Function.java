4909405ce63be2a766b00c38ee3a7873ba03c21d
patrickmylius <patrickmylius@outlook.dk>
2020-09-30 20:28:06 +0200

    refractored souts in Hus package Function

--- Programmering/src/Undervisning_30_09_2020/Hus/Function.java
+++ Programmering/src/Undervisning_30_09_2020/Hus/Function.java
@@ -3,8 +3,8 @@ package Undervisning_30_09_2020.Hus;
 public class Function {
     //method house status, returns String
     static String houseStatus(String answer) {
-        String houseLocked = "HOUSE LOCKED:\nMain door is unlocked\nMain door is open\nBackdoor is unlocked\nBasement door is unlocked";
-        String houseUnlocked = "HOUSE UNLOCKED:\nMain door is locked\nMain door is closed\nBackdoor is locked\nBasement door is locked";
+        String houseLocked = "HOUSE UNLOCKED:\nMain door is unlocked\nMain door is open\nBackdoor is unlocked\nBasement door is unlocked";
+        String houseUnlocked = "HOUSE LOCKED:\nMain door is closed\nMain door is locked\nBackdoor is locked\nBasement door is locked";
 
         //When user input equals "yes", run home method and return houseUnlocked
         //Any other user input, runs lockHouse method return houseLocked
