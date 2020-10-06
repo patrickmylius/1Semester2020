8942ecca64059a3e884e4d9e031ac3d6d59253db
patrickmylius <patrickmylius@outlook.dk>
2020-10-06 09:07:22 +0200

    addedW3School Integer ArrayList with notes.

--- Programmering/src/Undervisning10_05_10_2020/ArraList_Integer_W3School.java
+++ Programmering/src/Undervisning10_05_10_2020/ArraList_Integer_W3School.java
@@ -8,18 +8,27 @@
 package Undervisning10_05_10_2020;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class ArraList_Integer_W3School {
     public static void main(String[] args) {
+
+        //Declares arrayList "myNumbs"
+        //fills with numbs, 10,15,20,25 and 35
+        //souts all numbs.
         ArrayList<Integer> myNumbs = new ArrayList<Integer>();
-        myNumbs.add(10);
         myNumbs.add(15);
-        myNumbs.add(20);
+        myNumbs.add(10);
         myNumbs.add(25);
-        myNumbs.add(30);
+        myNumbs.add(20);
+        myNumbs.add(35);
+        //sorts numbs from lowest int to highest.
+        Collections.sort(myNumbs);
         for (int i : myNumbs) {
             System.out.println(i);
         }
+
 
     }
 }
