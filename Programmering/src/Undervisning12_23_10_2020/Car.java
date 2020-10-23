7d8c6af95ec2cbf4e36734ccae87a9f56c0d8b5f
patrickmylius <patrickmylius@outlook.dk>
2020-10-23 10:08:19 +0200

    Added Undervisning12_23_10_2020
    Containing class Car (holds car contructor)
    
    Containing class Garage (Using array to instantiate 1million cars in 5 diff colors)

---
+++ Programmering/src/Undervisning12_23_10_2020/Car.java
@@ -0,0 +1,17 @@
+/**
+ * Developed by Patrick Grønvold
+ * patr6996@edu.easj.dk
+ * Student of Computer Science
+ * Zealand ErhvervsAkademi - 4700
+ * 23-10-2020
+ */
+package Undervisning12_23_10_2020;
+
+public class Car {
+    String color;
+
+    //Constructer of Car, takes color
+    public Car(String color) {
+        this.color = color;
+    }
+}
---
+++ Programmering/src/Undervisning12_23_10_2020/Garage.java
@@ -0,0 +1,30 @@
+/**
+ * Developed by Patrick Grønvold
+ * patr6996@edu.easj.dk
+ * Student of Computer Science
+ * Zealand ErhvervsAkademi - 4700
+ * 23-10-2020
+ */
+package Undervisning12_23_10_2020;
+
+public class Garage {
+    static String[] color = {"blue", "red", "green", "yellow", "black"};
+
+    public static void main(String[] args) {
+
+        Car[] cars = new Car[1000000]; //creates var array with 1mil slots.
+        //fori loop creating 100000 blue cars.
+        for (int i = 0; i < cars.length ; i++) {
+            //fills array with 1 mil cars of diff colors.
+            cars[i] = new Car(randomColor()); //creates a new car for every i from 0-100000
+            System.out.println(i + 1 + ". "); //sout arr numb + 1.
+            System.out.println(cars[i].color);//Sout random generated car color.
+        }
+    }
+    //Method returning randomly between colors from String[] "color"
+    //Colors: blue, red, green, yellow and black.
+    static String randomColor() {
+        double x = Math.random() * 5;
+        return color[(int)x];
+    }
+}
